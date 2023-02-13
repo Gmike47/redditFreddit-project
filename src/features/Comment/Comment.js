2836a673258b4f4afe108c503b24f40b42ff1334
gal <galezra20@gmail.com>
2023-02-13 10:30:23 +0200

    fix profile name

--- src/features/Comment/Comment.js
+++ src/features/Comment/Comment.js
@@ -2,14 +2,14 @@ import React from "react";
 import moment from 'moment';
 import ReactMarkDown from 'react-markdown';
 import './Comment.css';
-import Avatar from '../Avatar/Avatar';
+import ProfilePic from '../PP/ProfilePic';
 
 const Comment = (props) => {
     const { comment } = props;
     return (
         <div className="comment">
             <div className="comment-metadata">
-                <Avatar name={comment.author} />
+                <ProfilePic name={comment.author} />
                 <p className="comment-author">{comment.author}</p>
                 <p className="comment-created-time">{moment.unix(comment.created_utc).fromNow()}</p>
             </div>
