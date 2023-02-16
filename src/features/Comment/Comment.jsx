417553d015954c492a4d5519ed0c34f322d14441
gal <galezra20@gmail.com>
2023-02-16 10:35:06 +0200

    change extension, fix spelling

---
+++ src/features/Comment/Comment.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import moment from 'moment';
+import ReactMarkdown from 'react-markdown';
+import './Comment.css';
+import ProfilePic from '../PP/ProfilePic';
+
+const Comment = (props) => {
+    const { comment } = props;
+    return (
+        <div className="comment">
+            <div className="comment-metadata">
+                <ProfilePic name={comment.author} />
+                <p className="comment-author">{comment.author}</p>
+                <p className="comment-created-time">{moment.unix(comment.created_utc).fromNow()}</p>
+            </div>
+            <ReactMarkdown source={comment.body} />
+        </div>
+    );
+};
+
+export default Comment;
