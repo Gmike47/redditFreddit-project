07a93b1b0d34f3d5bc306c264d5926b4eeba6e75
gal <galezra20@gmail.com>
2023-02-18 11:39:36 +0200

    fix prev

--- src/index.jsx
+++ src/index.jsx
@@ -4,7 +4,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import store from './store/store';
-import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -14,5 +13,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-serviceWorker.unregister();
