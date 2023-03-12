d8f1af15adc279fd7b0229cc1ee90e80343ade43
gal <galezra20@gmail.com>
2023-03-12 09:14:48 +0200

    change PP to Avatar

--- src/features/Comment/Comment.jsx
+++ src/features/Comment/Comment.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import moment from 'moment';
 import ReactMarkdown from 'react-markdown';
 import './Comment.css';
-import ProfilePic from '../PP/ProfilePic';
+import Avatar from '../Avatar/Avatar';
 
 const Comment = (props) => {
     const { comment } = props;
     return (
         <div className="comment">
             <div className="comment-metadata">
-                <ProfilePic name={comment.author} />
+                <Avatar name={comment.author} />
                 <p className="comment-author">{comment.author}</p>
                 <p className="comment-created-time">{moment.unix(comment.created_utc).fromNow()}</p>
             </div>
